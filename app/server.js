0d36ddab356a654af96a6f8bc72efe0ed6edcdce
Sabbas Ahmad <sabbbas.a30@gmail.com>
2025-08-07 13:54:49 +0500

    completing Initials

--- app/server.js
+++ app/server.js
@@ -5,6 +5,9 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the Vision Index Backend');
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
