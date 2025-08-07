68783c5f5a7a8ac3f45fdcc3547848f215d124a9
Sabbas Ahmad <sabbbas.a30@gmail.com>
2025-08-07 17:58:18 +0500

    Eslint check

--- app/server.js
+++ app/server.js
@@ -12,7 +12,7 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true,
 }).then(() => {
   console.log('Connected to MongoDB');
-}).catch((err) => {
+}).catch(err => {
   console.error('MongoDB connection error:', err);
 });
 
