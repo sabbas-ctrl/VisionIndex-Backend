f51fb8cf0d2dd449971f187dbe287f59d17ed8c2
Sabbas Ahmad <sabbbas.a30@gmail.com>
2025-08-07 13:36:30 +0500

    Initial Setup

---
+++ app/server.js
@@ -0,0 +1,12 @@
+// app/server.js
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+})
